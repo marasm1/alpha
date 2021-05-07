bccb42e88c986a9af7b0e65dbacb9efd7242e7f4
marasm1 <83776123+marasm1@users.noreply.github.com>
2021-05-07 08:27:58 -0500

    Update setup1.sh

--- setup1.sh
+++ setup1.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-apt update && apt upgrade
+apt install unzip wpasupplicant
+systemctl start wpa_supplicant
+systemctl enable wpa_supplicant
+
+#modify following for correct names and locations, add other ones
+sed -i 's/#HandleLidClosewhatwver/HandleLidCloseignore' /etc/systems/logind.conf
+
+#add line to sshd conf for root login
+
+
+
+#backup netplan file and create new one with correct data
+mv /etc/netplan/neplangggg.conf netplanggg.conf.bak
