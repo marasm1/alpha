3c026d47211c58fa4c0c23832882f4250c55782f
marasm1 <83776123+marasm1@users.noreply.github.com>
2021-05-07 20:42:08 -0500

    Create setup_miner.sh

---
+++ setup_miner.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip -P xlarig/
+unzip /home/administrator/xlarig/XLArig-v5.2.2-linux-x86_64.zip
+rm XLArig-v5.2.2-linux-x86_64.zip
